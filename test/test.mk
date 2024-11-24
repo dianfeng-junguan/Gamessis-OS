b1d2faf5065df59b2aa5d99f24cf16e3aafa10e4
xexux <1770711918@qq.com>
2024-11-24 19:48:12 +0800

    引进了别人的轻量级libc

--- test/test.mk
+++ test/test.mk
@@ -1,7 +1,7 @@
 .PHONY: test
 test:
 	gcc -c -g test/test.c -o bin/test.o -nostdlib -nostdinc -I include -nostartfiles -nodefaultlibs -fno-stack-protector -w
-	ld bin/test.o bin/libc.a -O elf64-x86-64 -o bin/test.elf -e main
+	ld bin/test.o bin/gmslibc.a -O elf64-x86-64 -o bin/test.elf -e main
 	objcopy -I binary bin/test.elf -O elf64-x86-64 bin/test.o -B i386
 cptest:
 	sudo mount rd.img /mnt
